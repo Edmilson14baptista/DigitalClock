437f048db7c760c68936c0e0dc1330a82579e517
Edmiilson Baptista <50552694+Edmilson14baptista@users.noreply.github.com>
2025-03-08 16:33:27 +0100

    Add files via upload

---
+++ src/assets/clock.js
@@ -0,0 +1,18 @@
+function updateClock() {
+    const now = new Date();
+
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('time').innerText = `${hours}:${minutes}:${seconds}`;
+
+    const daysOfWeek = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const day = daysOfWeek[now.getDay()];
+    const date = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const year = now.getFullYear();
+    document.getElementById('date').innerText = `${day}, ${date}/${month}/${year}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
